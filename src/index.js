b066c2ca2b5a0e69ff4eea8786f76df0d55a2b8b
Tatsuya <yokokura0413@gmail.com>
2021-01-05 08:30:39 +0900

    add: Exercises 1.1.-1.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
+const Header = props => (
+  <h1>{props.course}</h1>
 )
+
+const Content = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
+)
+
+const Total = (props) => (
+  <p>
+    {props.total}
+  </p>
+)
+
+const App = () => {
+  return (
+    <div>
+      <Header course={'Half Stack application development'} />
+      <Content part={'Fundamentals of React'} exercises={10} />
+      <Content part={'Using props to pass data'} exercises={7} />
+      <Content part={'State of a component'} exercises={14} />
+      <Total total={31} />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
