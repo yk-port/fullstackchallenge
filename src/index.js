1a6d10dfdef3d8ef5111e60c324506f591c797c7
Tatsuya <yokokura0413@gmail.com>
2021-01-06 06:56:59 +0900

    finish: 1.4 step4

--- src/index.js
+++ src/index.js
@@ -1,30 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = props => (
-  <h1>{props.course}</h1>
+const Header = ({ course }) => (
+  <h1>{course}</h1>
 )
 
-const Content = ({ part, exercises }) => (
-  <p>
-    {part} {exercises}
-  </p>
+const Content = ({ parts }) => (
+  <>
+    <h2>Content</h2>
+    <div>
+      {parts[0].name} {parts[0].exercises}
+    </div>
+    <div>
+      {parts[1].name} {parts[1].exercises}
+    </div>
+    <div>
+      {parts[2].name} {parts[2].exercises}
+    </div>
+  </>
 )
 
-const Total = (props) => (
-  <p>
-    {props.total}
-  </p>
-)
+const Total = ({ parts }) => {
+  let total = 0
+  for (const part of parts) {
+    total += part.exercises
+  }
+  return (
+    <>
+      <h2>Total</h2>
+      <p>{total}</p>
+    </>
+  )
+}
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <div>
-      <Header course={'Half Stack application development'} />
-      <Content part={'Fundamentals of React'} exercises={10} />
-      <Content part={'Using props to pass data'} exercises={7} />
-      <Content part={'State of a component'} exercises={14} />
-      <Total total={31} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
